587af0444c4d933c065272286fb1279007c9dc2b
koukemo <java.sparrow22.surface@gmail.com>
2019-01-31 11:50:19 +0900

    kotlin

---
+++ app/src/main/java/sample/qiitaclient/MainActivity.kt
@@ -0,0 +1,82 @@
+package sample.qiitaclient
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
+import android.widget.ProgressBar
+import com.trello.rxlifecycle.components.support.RxAppCompatActivity
+import com.trello.rxlifecycle.kotlin.bindToLifecycle
+import rx.android.schedulers.AndroidSchedulers
+import rx.schedulers.Schedulers
+import sample.qiitaclient.client.ArticleClient
+import javax.inject.Inject
+
+import com.google.gson.FieldNamingPolicy
+import com.google.gson.GsonBuilder
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+import sample.qiitaclient.model.Article
+import sample.qiitaclient.model.User
+import sample.qiitaclient.view.ArticleView
+import android.support.v7.widget.DialogTitle
+import rx.Scheduler
+
+
+class MainActivity : RxAppCompatActivity() {
+
+    @Inject
+    lateinit var articleClient: ArticleClient
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        (application as QiitaClientApp).component.inject(this)
+        setContentView(R.layout.activity_main)
+
+        val listView: ListView = findViewById(R.id.list_view) as ListView
+        val progressBar = findViewById(R.id.progress_bar) as ProgressBar
+        val queryEditText = findViewById(R.id.query_edit_text) as EditText
+        val searchButton = findViewById(R.id.search_button) as Button
+
+        val listAdapter = ArticleListAdapter(applicationContext)
+        listView.adapter = listAdapter
+        listView.setOnItemClickListener { adapterView, view, position, id ->
+            val intent = ArticleActivity.intent(this, listAdapter.articles[position])
+            startActivity(intent)
+        }
+
+        /*val gson = GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+                .create()
+        val retrofit = Retrofit.Builder()
+                .baseUrl("https://qiita.com")
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .build()
+        val articleClient = retrofit.create(ArticleClient::class.java)
+        */
+
+        searchButton.setOnClickListener {
+            progressBar.visibility = View.VISIBLE
+
+            articleClient.search(queryEditText.text.toString())
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .doAfterTerminate {
+                        progressBar.visibility = View.GONE
+                    }
+                    .bindToLifecycle(this)
+                    .subscribe({
+                        queryEditText.text.clear()
+                        listAdapter.articles = it
+                        listAdapter.notifyDataSetChanged()
+                    }, {
+                        toast("エラー: $it")
+                    })
+        }
+    }
+}
---
+++ app/src/main/java/sample/qiitaclient/QiitaClientApp.kt
@@ -0,0 +1,13 @@
+package sample.qiitaclient
+
+import android.app.Application
+import sample.qiitaclient.dagger.AppComponent
+import sample.qiitaclient.dagger.DaggerAppComponent
+
+
+class QiitaClientApp: Application() {
+
+    val component: AppComponent by lazy {
+        DaggerAppComponent.create()
+    }
+}
